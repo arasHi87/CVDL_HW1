6b20a383521defd731d06cc001697147324ff47e
arashi87 <arasi27676271@gmail.com>
2021-11-22 20:54:17 +0800

    refactor: rewrite gaussian use scipy instead written by double for loop (it will more fast)

--- src/module/detection.py
+++ src/module/detection.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
+from scipy import signal
 
 from .display import DisplayWindow
 
 
 class Detection:
     def __init__(self, parent=None):
-        self.img = cv2.imread("data/House.jpg")
+        self.img = cv2.imread("data/House.jpg", cv2.IMREAD_GRAYSCALE)
         self.display = DisplayWindow(parent)
 
-    def gaussian_blur(self):
+    def _gaussian_blur(self):
         kernel = np.array(
             [
                 [0.045, 0.122, 0.045],
@@ -17,26 +18,14 @@ class Detection:
                 [0.045, 0.122, 0.045],
             ]
         )
-        img = cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY)
-        k_h, k_w = kernel.shape  # kernel heigh and width
-        o_h, o_w = img.shape  # origin heigh and width
-        p_h, p_w = (k_h - 1) // 2, (k_w - 1) // 2  # pad heigh and width
-        result = np.zeros(shape=(o_h, o_w), dtype=np.uint8)
-        img = np.pad(
-            img,
-            pad_width=[(p_h, p_h), (p_w, p_w)],
-            mode="constant",
-            constant_values=0,
+        return signal.convolve2d(self.img, kernel, mode="same", boundary="symm").astype(
+            np.uint8
         )
 
-        for i in range(o_h):
-            for j in range(o_w):
-                result[i, j] = np.sum(
-                    np.multiply(img[i : i + k_h, j : j + k_w], kernel),
-                    dtype=np.uint8,
-                )
-
-        self.display.add_img_to_window(cv2.cvtColor(result, cv2.COLOR_GRAY2BGR))
+    def gaussian_blur(self):
+        result = self._gaussian_blur()
+        result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+        self.display.add_img_to_window(result)
         self.display.show()
 
     def sobel_x(self):
