e3026757cfcdd49f885abfa810d078f445e25049
arashi87 <arasi27676271@gmail.com>
2021-11-22 22:59:41 +0800

    refactor: rewrite sobel filter by scipy

--- src/module/detection.py
+++ src/module/detection.py
@@ -30,38 +30,28 @@ class Detection:
 
     def sobel_x(self):
         kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-        img = cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY)
-        o_h, o_w = img.shape  # origin heigh and width
-        k_h, k_w = kernel.shape  # kernel heigh and width
-        result = np.zeros(shape=(o_h, o_w), dtype=np.uint8)
-
-        for i in range(1, o_h - 2):
-            for j in range(1, o_w - 2):
-                result[i, j] = abs(
-                    np.sum(
-                        np.multiply(img[i : i + k_h, j : j + k_w], kernel),
-                    )
-                )
-
-        self.display.add_img_to_window(cv2.cvtColor(result, cv2.COLOR_GRAY2BGR))
+        result = signal.convolve2d(
+            self._gaussian_blur(), kernel, mode="same", boundary="symm"
+        )
+        result = abs(result)
+        result = (
+            (result - np.amin(result)) * 255 / (np.amax(result) - np.amin(result))
+        ).astype(np.uint8)
+        result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+        self.display.add_img_to_window(result)
         self.display.show()
 
     def sobel_y(self):
         kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
-        img = cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY)
-        o_h, o_w = img.shape  # origin heigh and width
-        k_h, k_w = kernel.shape  # kernel heigh and width
-        result = np.zeros(shape=(o_h, o_w), dtype=np.uint8)
-
-        for i in range(1, o_h - 2):
-            for j in range(1, o_w - 2):
-                result[i, j] = abs(
-                    np.sum(
-                        np.multiply(img[i : i + k_h, j : j + k_w], kernel),
-                    )
-                )
-
-        self.display.add_img_to_window(cv2.cvtColor(result, cv2.COLOR_GRAY2BGR))
+        result = signal.convolve2d(
+            self._gaussian_blur(), kernel, mode="same", boundary="symm"
+        )
+        result = abs(result)
+        result = (
+            (result - np.amin(result)) * 255 / (np.amax(result) - np.amin(result))
+        ).astype(np.uint8)
+        result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+        self.display.add_img_to_window(result)
         self.display.show()
 
     def magnitude(self):
